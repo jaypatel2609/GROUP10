f0f5c8ad24d412186f3bae5d24ac20f41d58befd
Prabikjot-Singh <68027007+Prabikjot-Singh@users.noreply.github.com>
2020-07-16 14:28:23 -0700

    Update AngryDuck.java

--- AngryDuck.java
+++ AngryDuck.java
@@ -1,5 +1,11 @@
-public class AngryDuck extends WaterFowl
+import java.util.Random;
+public class AngryDuck extends WaterFowl implemnets Teleporter
 {
+
+     private int X = 0;
+     private int Y = 501;
+     private Random generator = new Random();
+     
      public AngryDuck()
      {
           
@@ -8,6 +14,21 @@ public class AngryDuck extends WaterFowl
      public AngryDuck(String name, String color)
      {
           super(name,color);
+     }
+     
+     public void teleport()
+     {
+          this.X = generator.nextInt(1000) + 1;
+          this.Y = generator.nextInt(500) + 501; 
+          System.out.println(this.name + " teleported.");
+     }
+     public String getPosition()
+     {
+          String pos = "";
+          
+          pos += "(" + this.X + ", " + this.Y + ")";
+          
+          return pos;
      }
 
 }
